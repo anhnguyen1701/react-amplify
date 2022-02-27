4b249003688e767e5d765b101de1d540194d79a0
anh nguyen <nguyenlamanh1701@gmail.com>
2022-02-27 07:56:09 +0700

    cm

---
+++ src/App.js
@@ -0,0 +1,119 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+
+import { Auth, Hub } from 'aws-amplify';
+
+const inititalFormState = {
+  username: '',
+  password: '',
+  email: '',
+  authCode: '',
+  formType: 'signUp',
+};
+
+function App() {
+  const [formState, updateFormState] = useState(inititalFormState);
+  const [user, updateUser] = useState('');
+
+  useEffect(() => {
+    checkUser();
+    setAuthListener();
+  }, []);
+
+  async function setAuthListener() {}
+
+  async function checkUser() {
+    try {
+      const user = await Auth.currentAuthenticatedUser();
+      console.log('user: ', user);
+      updateUser(user);
+      updateFormState(() => ({ ...formState, formType: 'signedIn' }));
+    } catch (error) {
+      updateUser(null);
+    }
+  }
+
+  function onChange(e) {
+    e.persist();
+    updateFormState(() => ({
+      ...formState,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  const { formType } = formState;
+
+  async function signUp() {
+    const { username, email, password } = formState;
+    await Auth.signUp({ username, password, attributes: { email } });
+    updateFormState(() => ({
+      ...formState,
+      formType: 'confirmSignUp',
+    }));
+  }
+  async function confirmSignUp() {
+    const { username, authCode } = formState;
+    await Auth.confirmSignUp(username, authCode);
+    updateFormState(() => ({ ...formState, formType: 'signIn' }));
+  }
+
+  async function signIn() {
+    const { username, password } = formState;
+    await Auth.signIn({ username, password });
+    updateFormState(() => ({ ...formState, formType: 'signedIn' }));
+  }
+
+  async function signOut() {
+    await Auth.signOut();
+    window.location.reload();
+  }
+
+  return (
+    <>
+      <div className="App">
+        {formType === 'signUp' && (
+          <div>
+            <input name="username" onChange={onChange} placeholder="username"></input>
+            <input name="password" onChange={onChange} placeholder="password"></input>
+            <input name="email" onChange={onChange} placeholder="email"></input>
+            <button onClick={signUp}>Sign Up</button>
+            <button
+              onClick={() =>
+                updateFormState(() => ({
+                  ...formState,
+                  formType: 'signIn',
+                }))
+              }
+            >
+              Sign In
+            </button>
+          </div>
+        )}
+
+        {formType === 'confirmSignUp' && (
+          <div>
+            <input name="authCode" onChange={onChange} placeholder="Confirmation code"></input>
+            <button onClick={confirmSignUp}>Confirm Sign Up</button>
+          </div>
+        )}
+
+        {formType === 'signIn' && (
+          <div>
+            <input name="username" onChange={onChange} placeholder="username"></input>
+            <input name="password" type="password" onChange={onChange} placeholder="password"></input>
+            <button onClick={signIn}>Sign In</button>
+          </div>
+        )}
+
+        {formType === 'signedIn' && (
+          <div>
+            <h1>Hello user</h1>
+            <button onClick={signOut}>Sign Out</button>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default App;
